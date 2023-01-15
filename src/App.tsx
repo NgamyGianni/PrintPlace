aaa4a4c69815ed1aaddef2d64edce19682af6bb7
NgamyGianni <36660655+NgamyGianni@users.noreply.github.com>
2023-01-15 12:01:31 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Main } from './components/home'
+
+function App() {
+
+  return (
+      <Main />
+  )
+}
+
+export default App
