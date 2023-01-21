43a90af69fc99818bb0b1829350e787cd63614d5
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2023-01-21 19:20:56 +0100

    Added logged in component in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,40 @@
-import { Navbar, Text, Button, Link } from '@nextui-org/react';
+import { Navbar, Text, Button, Link, Avatar } from "@nextui-org/react"
 
 export const HeaderNavbar = () => {
-    const pages : object = {"Try" : "#", "Explore" : "#"};
+	const pages: object = { Try: "#", Explore: "#" }
+	const logged = true
+	const contentPages = (pages: object) => Object.entries(pages).map((k) => <Navbar.Link href={k[1]}>{k[0]}</Navbar.Link>)
 
-    const contentPages = (pages : object) => Object.entries(pages).map((k) => <Navbar.Link href={k[1]}>{k[0]}</Navbar.Link>)
-    
-    return (<Navbar isBordered variant="floating">
-        <Navbar.Brand>
-          <Text b color="inherit" hideIn="xs">
-            DesignR
-          </Text>
-        </Navbar.Brand>
-        <Navbar.Content hideIn="xs">
-          {contentPages(pages)}
-        </Navbar.Content>
-        <Navbar.Content>
-          <Navbar.Link color="inherit" href="#">
-            Login
-          </Navbar.Link>
-          <Navbar.Item>
-            <Button auto flat as={Link} href="#">
-              Sign Up
-            </Button>
-          </Navbar.Item>
-        </Navbar.Content>
-      </Navbar>)
+	return (
+		<Navbar isBordered variant="floating">
+			<Navbar.Brand>
+				<Text b color="inherit" hideIn="xs">
+					DesignR
+				</Text>
+			</Navbar.Brand>
+			<Navbar.Content hideIn="xs">{contentPages(pages)}</Navbar.Content>
+			<Navbar.Content>
+				{!logged && (
+					<>
+						<Navbar.Link color="inherit" href="#">
+							Login
+						</Navbar.Link>
+						<Navbar.Item>
+							<Button auto flat as={Link} href="#">
+								Sign Up
+							</Button>
+						</Navbar.Item>
+					</>
+				)}
+				{logged && (
+					<>
+						<Navbar.Item>
+							<Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" size="lg" />
+						</Navbar.Item>
+						<Navbar.Item>TheMaoZedong</Navbar.Item>
+					</>
+				)}
+			</Navbar.Content>
+		</Navbar>
+	)
 }
